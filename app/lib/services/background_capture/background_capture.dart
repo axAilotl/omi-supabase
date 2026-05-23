ca1b3416c04371b42c0582d2f0bdba51de68db3c
axAilotl <231548431+axAilotl@users.noreply.github.com>
2026-05-22 22:34:23 -0400

    Integrate Android background capture slices

--- app/lib/services/background_capture/background_capture.dart
+++ app/lib/services/background_capture/background_capture.dart
@@ -1,3 +1,4 @@
 export 'package:omi/services/background_capture/background_capture_models.dart';
 export 'package:omi/services/background_capture/background_capture_pipeline.dart';
 export 'package:omi/services/background_capture/background_capture_service.dart';
+export 'package:omi/services/background_capture/background_capture_state.dart';
